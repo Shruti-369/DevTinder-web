e98ed7f6c772dcd74a895c4f5ec3af7cd58ca9d4
Shruti-369 <alewashruti@gmail.com>
2026-06-10 22:51:41 +0530

    reload

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useEffect, useNavigate } from 'react';
 import NavBar from './NavBar';
 import Footer from './Footer';
 import { Outlet } from 'react-router-dom';
+import axios from 'axios';
+import { BASE_URL } from '../utils/constants';
+import { useDispatch } from 'react-redux';
+import { addUser } from '../utils/userSlice';
 
 const Body = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
+  const fetchUser = async () => {
+    try {
+      const res = await axios.get(BASE_URL + "/profile/view", {
+        withCredentials: true,
+      }); dispatch(addUser(res.data));
+    } catch (err) {
+      navigate("/login");
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    fetchUser();
+  }, []);
+
   return (
     <div className="min-h-screen flex flex-col bg-base-100 text-base-content pb-16">
       <NavBar />
